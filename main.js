f5e924b91131539b17f45512a0306be7e417c1e5
diego <diegozaratean@gmail.com>
2024-03-04 15:44:28 +0000

    finish class

--- main.js
+++ main.js
@@ -1,105 +1,93 @@
-console.log('desde archivo 123')
+console.log('Loops y Arrays')
+
+// Array : Mario Luigi Bowser Toad Peach
+let personajesMario = ['Mario','Luigi','Bowser','Toad','Peach']
 
 
-function saludo(){
-    console.log('Hola')
+
+let personajesBuenos = []
+for(let i =  0 ; i < personajesMario.length ; i++){
+    console.log(personajesMario[i])
+    if(personajesMario[i] != 'Bowser'){
+        // copiar
+        personajesBuenos.push(personajesMario[i])
+    }
 }
 
-saludo()
-// saludo()
-// saludo()
+console.log(personajesMario)
+console.log(personajesBuenos)
 
-// function transportar(origen,destino){
-//     console.log('Saliendo de ' + origen)
-//     console.log('moviendome')
-//     console.log('llegando a ' + destino)
-// }
-// transportar('casa','super')
-// transportar('super','oficina')
-// transportar('oficina','casa')
-// transportar()
-// transportar()
+let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-// let alumno = 'juan'
-// console.log('Andes de la funcion ' + alumno)
-// function saludarAlumno(alumno){
-//     console.log('Dentro de la funcion ' + alumno)
-//     console.log('Hola ' + alumno)
-// }
+console.log('Con FILTER')
+console.log(personajesMario.filter( (element)=> element != 'Bowser' ))
+console.log(personajesMario.filter( (personaje)=> personaje != 'Bowser' ))
 
-// console.log('Despues de la funcion ' + alumno)
+console.log(numbers.filter( (number)=> number > 7 ))
+console.log(numbers.filter( (number)=> number > 7 && number <= 15  ))
+console.log(numbers.filter( (number)=> number%3 == 0 ))
 
-// saludarAlumno('pedro')
-// saludarAlumno('jorge')
-// saludarAlumno('Cris')
 
-function suma(num1,num2){
-    return num1 + num2
+console.log(numbers)
+let newnumbers = []
+for(i = 0; i < numbers.length; i++){
+    newnumbers.push(numbers[i] * 4)
 }
 
+console.log(newnumbers)
 
-// console.log(suma(2,2), 'esto debe devolver 4') /// 4
-// console.log(suma(2,5))  // 7
-// console.log(suma(6,3)) // 9
-// console.log(suma(0,5)) // 5
 
-// function puedoBeber(edad){
-//     if(edad >= 18){
-//         console.log(1)
-//         return 'Si puedes beber'
-//         console.log(2)
-//     }else{
-//         console.log(3)
-//         return 'No puedes beber'
-//         console.log(4)
+console.log('CON MAP')
+console.log(numbers.map( x => x * 4 ))
+console.log(  personajesMario.map( j => j + 's'  ) )
+
+personajesBuenos.forEach( (item) => {
+    console.log(item)
+})
+
+console.log('Do While')
+let j = 20
+do {
+    console.log(j)
+    j++
+}while( j < 10)
+// console.log(personajesMario[0])
+// console.log(personajesMario[1])
+// console.log(personajesMario[2])
+// console.log(personajesMario[3])
+// console.log(personajesMario[4])
+
+
+// i++. => i = i + 1
+// for(incializacion;condicion;acutalizacion)
+// i = 6 ;  > 0
+// for(let i = personajesMario.length - 1 ; i >= 0 ; i--){
+//     console.log(personajesMario[i])
+// }
+
+// 0 1 2 3 4 
+// 4 3 2 1 0
+
+// console.log(numbers)
+// // 0 1 2 3 4 5 6 7 8 9 .. 19.  20 veces
+// for(let i = 0; i < numbers.length;i++){
+//     console.log('me estoy ejecutando',i)
+//     if(i %3 === 0){
+//         console.log('el valor de i',i)
+//         console.log('el valor de number ',numbers[i])
 //     }
 // }
 
-// console.log(puedoBeber("24"))
-// console.log(puedoBeber(14))
-
-function calculoIRPF(ingreso){
-    return ingreso * 0.15
-}
-
-
-
-document.getElementById('impuesto').innerHTML = calculoIRPF(10000)
-
-
-
-
-// console.log('ANTES se ejecuto window onload');
-// window.onload = function() {
-
-//     console.log('se ejecuto window onload');
-
+// // 0 3 6 9 12 15 18  7 veces
+// for(let i = 0; i < numbers.length;i = i + 3){
+//     console.log('me estoy ejecutando',i)
+//     console.log('el valor de i',i)
+//     console.log('el valor de number ',numbers[i])
 // }
-// console.log('DESPUES se ejecuto window onload');
 
-// funcion declarativa
-saludoDeclarativo()
-function saludoDeclarativo(){
-    console.log('Hola saludoDeclarativo')
-}
-
-
-// funcion de expresión
-const saludExpresion = function(){
-    console.log('Hola saludExpresion')
-}
-saludExpresion()
-
-//funcion flecha
-const saludoArrow = ()=> {
-    console.log('Hola saludoArrow')
-}
-saludoArrow()
-
-// PSEUDO CODIGO
-// 1. modificar el html desde js
-// 2. armnar una oracion a aprtir de un sujeto un verbo y etc
-// 3. obetenr un el;emento aleatorio de who
-// 4. obetenr un el;emento aleatorio de action
-// 5. obetenr un el;emento aleatorio de what
-// 6. obetenr un el;emento aleatorio de whn
+// i+5 ==> 
+// i++. => i = i + 1
+// 30 35 40 45 50 
+// for(i = 30; i <= 50 ; i+=5){
+//     console.log(i)
+// }
